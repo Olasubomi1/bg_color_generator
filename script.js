9a548d5d9809c3fc48c61a3f79e809cb4a5c8ced
Olasubomi <shubbynlaa@gmail.com>
2022-07-19 19:18:32 +0100

    Implemented linear-gradient style to update in h3 textcontent.

--- script.js
+++ script.js
@@ -2,17 +2,15 @@ const css = document.querySelector("h3");
 const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
 const body = document.getElementById("gradient");
-console.log(body);
 
-color1.addEventListener("input", function () {
+function setGradient() {
   body.style.background = `linear-gradient(to right, 
-    ${color1.value}, 
-    ${color2.value}
-    )`;
-});
-color2.addEventListener("input", function () {
-  body.style.background = `linear-gradient(to right, 
-    ${color1.value}, 
-    ${color2.value}
+        ${color1.value}, 
+        ${color2.value}
         )`;
-});
+
+  css.textContent = `${body.style.background} ;`;
+}
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
